bc65861e7ec6f1f11506b3dcba3c881ae4603952
Yuktha0208 <71544862+Yuktha0208@users.noreply.github.com>
2021-05-05 11:29:22 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,45 @@
+song1="";
+song2="";
+
+leftWristX=0;
+leftWristY=0;
+rightWristX=0;
+rightWristY=0;
+
+function preload(){
+    song1=loadSound("music.mp3");
+    song2=loadSound("music2.mp3");
+}
+
+function setup(){
+    canvas=createCanvas(500,500);
+    canvas.center();
+    video=createCapture(VIDEO);
+    video.hide();
+
+    pose=ml5.poseNet(video,modelLoaded);
+    pose.on("pose",gotPoses);
+}
+
+function draw(){
+    image(video,0,0,500,500);
+}
+
+function modelLoaded(){
+    console.log("your Model Is Initiaized");
+}
+
+function gotPoses(results){
+    if(results.length > 0){
+        console.log(results);
+
+        leftWristX=results[0].pose.leftWrist.x;
+        leftWristY=results[0].pose.leftWrist.y;
+        rightWristX=results[0].pose.rightWrist.x;
+        rightWristY=results[0].pose.rightWrist.y;
+
+        console.log("leftWristX= "+leftWristX+",leftWristY= "+leftWristY);
+        console.log("rightWristX= "+rightWristX+",rightWristY= "+rightWristY);
+
+    }
+}
